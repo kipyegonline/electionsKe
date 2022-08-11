468bfcd9f7a029e0951b34fe7aa7f97a95f084d7
Vince <vinnykipx@gmail.com>
2022-08-11 19:07:14 +0300

    fifth build, probably the last

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,27 +22,35 @@ import {
   CardMedia,
   CardContent,
   TablePagination,
+  Alert,
+  Slide,
+  Snackbar,
 } from "@mui/material";
 import { InfoTwoTone } from "@mui/icons-material";
-const Home: NextPage = ({ data, county, stations, constituency }) => {
-  const [prs, setPrs] = React.useState(data);
+const Home: NextPage = ({
+  data,
+  county,
+  stations,
+  constituency,
+  constResults,
+}) => {
+  const [prs, setPrs] = React.useState(data); // presidential results
   const [loading, setLoading] = React.useState(false);
   const [search, setSearch] = React.useState("");
   const [err, setError] = React.useState("");
   const [type, setType] = React.useState("county");
-  const [count, setCount] = React.useState(48);
+  const [count, setCount] = React.useState(49); //pagination
+
   const [_county, setCounty] = React.useState(county);
   const [_constituency, setConst] = React.useState(constituency);
   const [_station, setStation] = React.useState(stations);
-  const [updating, setUpdating] = React.useState(false);
-  const [candidates, setCandidates] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [constituencyRes, setconstituencyRes] = React.useState([]);
-
-  const [cur, setCur] = React.useState({ prev: 0, next: count });
-
-  let timeout;
-
+  const [updating, setUpdating] = React.useState(false); // refreshing spinner
+  const [candidates, setCandidates] = React.useState([]); // for cards display
+  const [page, setPage] = React.useState(0); // current page on pagination
+  // just there
+  const [searched, setSearched] = React.useState(false);
+  let timeout!: number;
+  // end points
   let url1 = "https://static.nation.africa/2022/president.json";
   let url2 = "https://static.nation.africa/2022/county.json";
   let url3 = "https://static.nation.africa/2022/stations.json";
@@ -50,6 +58,7 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
   const link =
     "https://elections.nation.africa/images/candidates/2022/ruto.jpg";
 
+  // work horse f the app is this filter function, transforms data object ro arrays
   const filterPayload = (data = [], key = "COUNTY_NO", region = []) => {
     const clean = data.reduce((acc, item) => {
       const county = region.find((reg) => reg[key] === item[key]);
@@ -58,12 +67,14 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
       acc[`group_${item[key]}`].push(item);
       return acc;
     }, {});
-    // console.log(clean, "clean");
+
     return Object.values(clean);
   };
   /**    width: 70px;
     height: 70px;
  */
+
+  // function for making API request after component mounts
   const fetchData = async (url, setLoading, setData, setError, type) => {
     try {
       setLoading(true);
@@ -84,10 +95,12 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
       setLoading(false);
     }
   };
+
+  // button clicks
   const handleClick = (incomingType: string) => {
     return () => {
       clearInterval(timeout);
-      if (incomingType === type) return;
+      // if (incomingType === type) return;
       setType(incomingType);
       if (incomingType === "county")
         fetchData(url1, setLoading, setPrs, setError, "county");
@@ -95,6 +108,8 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
         fetchData(url1, setLoading, setPrs, setError, "constituency");
     };
   };
+
+  // search box
   const handleSearch = (search: string) => {
     clearInterval(timeout);
 
@@ -102,30 +117,31 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
     if (search) {
       if (type === "county") {
         info = data.filter((smallItem) => {
-          console.log(smallItem?.region?.COUNTY_NAME, search);
           return smallItem?.region?.COUNTY_NAME.toLowerCase()?.includes(
             search.toLowerCase()
           );
         });
-
+        console.log(info, "countiiiii");
         if (info.length) return setPrs(info);
-        else return alert("No results that " + type);
+        else setType("constituency");
       }
 
       if (type === "constituency") {
-        info = prs.filter((smallItem) => {
-          console.log(smallItem, search, constituencyRes, "small small");
-          return smallItem?.region?.CONSTITUENCY_NAME.toLowerCase()?.includes(
+        info = constituencyRes.filter((smallItem) => {
+          return smallItem?.[0]?.region?.CONSTITUENCY_NAME.toLowerCase()?.includes(
             search.toLowerCase()
           );
         });
-        console.log(info, "countiiiii");
 
-        if (info.length) return setPrs(info);
-        else return alert("No results that " + type);
+        if (info.length) return setPrs(info[0]);
+        else setType("county");
       }
+
+      if (info.length <= 0) setError("No results");
+      setTimeout(() => setError(""), 3000);
     }
   };
+
   React.useEffect(() => {
     const presurl = "https://static.nation.africa/2022/president.json";
     //fetchData(presurl, setLoading, setPrs, setError, "county");
@@ -133,14 +149,9 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
     //fetchData(url4, setLoading, setConst, setError, "");
     //fetchData(url3, setLoading, setStation, setError, "");
     //arrayHashmap(prs);
+
     fetchData(presurl, setUpdating, setCandidates, setError, "");
-    fetchData(
-      url1,
-      function (loading) {},
-      setconstituencyRes,
-      setError,
-      "constituency"
-    );
+    // request the server every one minute
     timeout = setInterval(() => {
       fetchData(url1, setUpdating, setPrs, setError, type);
       fetchData(presurl, setLoading, setCandidates, setError, "");
@@ -150,36 +161,57 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
       clearInterval(timeout);
     };
   }, []);
+
+  // pagination
   const handleChangePage = (e, page) => {
-    console.log(page);
     setPage(page);
   };
+
   const handleChangeRowsPerPage = (e) => {
     setCount(e.target.value);
     setPage(0);
   };
+  const [constituencyRes, setconstituencyRes] = React.useState(
+    filterPayload(constResults, "CONSTITUENCY_NO", constituency)
+  );
+
   const memoPRS = React.useMemo(() => {
     let key = type === "county" ? "COUNTY_NO" : "CONSTITUENCY_NO";
     const value = type === "county" ? "COUNTY_NAME" : "CONSTITUENCY_NAME";
     const region = type === "county" ? county : constituency;
+    //prs=prs.length===1 ? prs[0] :prs
     let payload = filterPayload(prs, key, region);
-    payload = payload.slice().sort((a, z) => {
-      console.log(a, z, "sortie");
+    /*
+    const pp = payload.slice().sort((a, z) => {
+      console.log(a[0].region[value], z[0].region[value], "sortie");
+      return (
+        a[0].region[value].toLowerCase() - z[0].region[value].toLowerCase()
+      );
     });
-
+    console.log(pp, "ppp");*/
     return payload;
   }, [prs]);
-  console.log(memoPRS, constituencyRes, "rsmmms");
+
   return (
     <div className={styles.container}>
+      {err && <SnackBar open={!!err} message={err} />}
       <Head>
-        <title>GENERAL ELECTIONS 2022</title>
-        <meta name="General Elections 2022" content="KE elections 2022" />
+        <title>PRESIDENTIAL ELECTIONS 2022</title>
+        <meta
+          name=" KENYA General Elections 2022"
+          content="KE elections 2022"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section>
+      <section
+        style={{
+          backgroundImage: "linear-gradient(to top, #accbee 0%, #e7f0fd 100%",
+          backgroundSize: "cover",
+        }}
+        className="py-2"
+      >
         {" "}
-        <Typography className="text-xl p-2 sm:p-4  mt-4 text-left sm:text-center">
+        <Typography className="text-xl p-2 sm:p-4  mt-2 text-left sm:text-center">
           General Elections 2022 Results:{" "}
           <b>{stations[0]?.PRESIDENTIALTOTAL?.toLocaleString()}</b> of {"  "}
           <b> {stations[1]?.STATIONTOTAL?.toLocaleString()}</b> polling stations
@@ -194,7 +226,7 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
         </Typography>
       </section>
       <main className={` ${styles.main} flex flex-col sm:flex-row`}>
-        <section className="w-full py-2 my-2  sm:my-4 sm:py-4 sm:w-1/4 ">
+        <section className="w-full py-0 my-0  sm:my-4 sm:py-4 sm:w-1/4 ">
           {updating ? (
             <Box className="my-auto py-2 text-center">
               <CircularProgress color="primary" size="2rem" thickness={4} />
@@ -213,8 +245,8 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
                 color="primary"
                 className={
                   type === "county"
-                    ? "bg-blue-500 text-white hover:bg-blue-500"
-                    : ""
+                    ? "bg-blue-500 text-white hover:bg-blue-500 w-10/12 sm:w-40"
+                    : " w-10/12 sm:w-60"
                 }
                 variant={type === "county" ? "contained" : "outlined"}
               >
@@ -227,8 +259,8 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
                 onClick={handleClick("constituency")}
                 className={
                   type === "constituency"
-                    ? "bg-blue-500 text-white hover:bg-blue-500"
-                    : ""
+                    ? "bg-blue-500 text-white hover:bg-blue-500 w-10/12 sm:w-40"
+                    : " w-10/12 sm:w-60"
                 }
                 variant={type === "constituency" ? "contained" : "outlined"}
               >
@@ -269,7 +301,7 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
         </section>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={`${styles.footer}  p-4 bg-purple-700 text-white`}>
         <a> Vince &copy; {new Date().getFullYear()} </a>
       </footer>
     </div>
@@ -278,7 +310,7 @@ const Home: NextPage = ({ data, county, stations, constituency }) => {
 
 export default Home;
 
-const SearchInput = ({ sendSearch }) => {
+const SearchInput = ({ sendSearch = (f: string) => f }) => {
   const [text, setText] = React.useState("");
   const handleSearch = () => {
     if (text) sendSearch(text);
@@ -289,7 +321,7 @@ const SearchInput = ({ sendSearch }) => {
         type="search"
         size="small"
         className="mb-4"
-        placeholder="Search county or constituency"
+        placeholder="county or constituency"
         onChange={(e) => setText(e.target.value)}
         onBlur={handleSearch}
       />
@@ -297,7 +329,7 @@ const SearchInput = ({ sendSearch }) => {
       <Button
         onClick={handleSearch}
         color="primary"
-        className="text-white ml-4 bg-blue-500 hover:bg-blue-500"
+        className="text-white ml-0 sm:ml-4 bg-blue-500 hover:bg-blue-500 w-11/12 sm:w-32"
       >
         Search
       </Button>
@@ -311,6 +343,8 @@ const ElectionsTable = ({ data, region, count }) => {
   let location = region === "County" ? "COUNTY_NO" : "CONSTITUENCY_NO";
   //data = region === "Constituency " ? data[0] : data;
   const { page, cur } = count;
+  const ruto = topLevel.find((item) => item.PRESIDENT_ID === 2);
+  const raila = topLevel.find((item) => item.PRESIDENT_ID === 1);
 
   return (
     <Box>
@@ -326,12 +360,16 @@ const ElectionsTable = ({ data, region, count }) => {
                   <div className="w-18 h-10">
                     <img
                       className="w-18 h-10"
-                      src={`${url}/${topLevel[0]?.SMALL_IMAGE}`}
+                      src={`${url}/${raila?.SMALL_IMAGE}`}
                     />
                   </div>
                   <div>
-                    {topLevel[0]?.FIRST_NAME} {topLevel[0]?.LAST_NAME}
+                    {raila?.FIRST_NAME} {raila?.LAST_NAME}
                   </div>
+                  <div
+                    style={{ background: `#${raila?.COLOURCODES}` }}
+                    className="h-4 w-4 ml-4"
+                  ></div>
                 </div>
               </TableCell>
               <TableCell>
@@ -339,13 +377,17 @@ const ElectionsTable = ({ data, region, count }) => {
                   <div className="w-18 h-10">
                     <img
                       className="w-18 h-10"
-                      src={`${url}/${topLevel[1]?.SMALL_IMAGE}`}
+                      src={`${url}/${ruto?.SMALL_IMAGE}`}
                     />
                   </div>
                   <div>
                     {" "}
-                    {topLevel[1]?.FIRST_NAME} {topLevel[1]?.LAST_NAME}
+                    {ruto?.FIRST_NAME} {ruto?.LAST_NAME}
                   </div>
+                  <div
+                    style={{ background: `#${ruto?.COLOURCODES}` }}
+                    className="h-4 w-4 ml-4"
+                  ></div>
                 </div>
               </TableCell>
               <TableCell>Total votes Cast</TableCell>
@@ -378,15 +420,15 @@ const ElectionTable = ({ data, i }) => {
   return (
     <TableRow key={i}>
       {" "}
-      <TableCell>{i + 1}</TableCell>
+      <TableCell>{i + 1}.</TableCell>
       <TableCell>{data[0]?.region?.COUNTY_NAME}</TableCell>
       <TableCell>
         {raila?.CANDIDATE_VOTES?.toLocaleString()}{" "}
-        <b className="pl-3 ml-4">({Math.ceil(raila?.COUNTY_PERCENTAGE)})%</b>
+        <b className="pl-3 ml-4">({raila?.COUNTY_PERCENTAGE?.toFixed(2)})%</b>
       </TableCell>
       <TableCell>
         {ruto?.CANDIDATE_VOTES?.toLocaleString()}{" "}
-        <b className="pl-3 ml-4">({Math.ceil(ruto?.COUNTY_PERCENTAGE)})%</b>
+        <b className="pl-3 ml-4">({ruto?.COUNTY_PERCENTAGE?.toFixed(2)})%</b>
       </TableCell>
       <TableCell>
         {data[1]?.TOTAL_PRESIDENT_COUNTY_VOTES_CAST?.toLocaleString()}
@@ -406,13 +448,13 @@ const ElectionTableConstituency = ({ data, i }) => {
       <TableCell>
         {raila?.CANDIDATE_VOTES?.toLocaleString()}{" "}
         <b className="pl-3 ml-4">
-          ({Math.ceil(raila?.CONSTITUENCY_PERCENTAGE)})%
+          ({raila?.CONSTITUENCY_PERCENTAGE?.toFixed(2)})%
         </b>
       </TableCell>
       <TableCell>
         {ruto?.CANDIDATE_VOTES?.toLocaleString()}
         <b className="pl-3 ml-4">
-          ({Math.ceil(ruto?.CONSTITUENCY_PERCENTAGE)})%
+          ({ruto?.CONSTITUENCY_PERCENTAGE?.toFixed(2)})%
         </b>
       </TableCell>
       <TableCell>
@@ -432,10 +474,10 @@ const CandidatesComponent = ({ candidatess }) => {
   } = candidatess;
   return (
     <Box className="flex flex-col gap-6">
-      <Card className="w-60 h-80">
+      <Card className="w-full sm:w-60 h-80 pt-2 my-4">
         <CardMedia
           component="img"
-          className="mx-auto"
+          className="mx-auto pt-2"
           image={`${url}/${raila.SMALL_IMAGE}`}
           alt="Raila"
           sx={{ width: 200, height: 140 }}
@@ -447,19 +489,26 @@ const CandidatesComponent = ({ candidatess }) => {
             {raila.LAST_NAME}
           </Typography>
 
-          <Typography>
-            Votes: {raila.CANDIDATE_VOTES.toLocaleString()}{" "}
-            <b className="ml-4"> ({Math.ceil(raila.NATIONAL_PERCENTAGE)} %)</b>
+          <Typography className="text-center">
+            Votes: {raila.CANDIDATE_VOTES.toLocaleString()} <br />
+            <b className="ml-4 text-2xl ">
+              {" "}
+              {raila.NATIONAL_PERCENTAGE?.toFixed(2)} %
+            </b>
           </Typography>
-          <Typography>
+          <Typography className="text-center">
             votes casted: {total_votes_cast.toLocaleString()}
           </Typography>
+          <div
+            className="h-6 w-full mt-4"
+            style={{ background: `#F5C700` }}
+          ></div>
         </CardContent>
       </Card>
-      <Card className="w-60 h-80">
+      <Card className="w-full sm:w-60 h-80 pt-2 my-4">
         <CardMedia
           component="img"
-          className="mx-auto"
+          className="mx-auto pt-2"
           sx={{ width: 200, height: 140 }}
           image={`${url}/${ruto.SMALL_IMAGE}`}
           alt="ruto"
@@ -469,13 +518,19 @@ const CandidatesComponent = ({ candidatess }) => {
           <Typography className="text-center" variant="h5">
             {ruto.FIRST_NAME} {ruto.LAST_NAME}
           </Typography>
-          <Typography>
-            Votes: {ruto.CANDIDATE_VOTES.toLocaleString()}
-            <b className="ml-4">({Math.ceil(ruto.NATIONAL_PERCENTAGE)} %)</b>
+          <Typography className="text-center">
+            Votes: {ruto.CANDIDATE_VOTES.toLocaleString()} <br />
+            <b className="ml-4 text-2xl">
+              {ruto.NATIONAL_PERCENTAGE?.toFixed(2)} %
+            </b>
           </Typography>
-          <Typography>
+          <Typography className="text-center">
             Votes casted: {total_votes_cast.toLocaleString()}
           </Typography>
+          <div
+            className="h-6 w-full mt-4"
+            style={{ background: `#031470` }}
+          ></div>
         </CardContent>
       </Card>
     </Box>
@@ -501,13 +556,41 @@ export async function getStaticProps() {
       res4.json(),
     ]);
     return {
-      props: { data: data.county.candidates, county, stations, constituency },
+      props: {
+        data: data.county.candidates,
+        county,
+        stations,
+        constituency,
+        constResults: data.constituency.candidates,
+      },
       revalidate: 10,
     };
   } catch (error) {
     console.log(error.message);
     return {
-      props: { data: [], county: [], stations: [], constituency: [] },
+      props: {
+        data: [],
+        county: [],
+        stations: [],
+        constituency: [],
+        constResults: [],
+      },
     };
   }
 }
+
+const SnackBar = ({ open, message }) => {
+  const TransitionComp = (props) => <Slide {...props} direction="down" />;
+  return (
+    <Snackbar
+      open={open}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      TransitionComponent={TransitionComp}
+      transitionDuration={500}
+    >
+      <Alert variant="filled" color="error">
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
